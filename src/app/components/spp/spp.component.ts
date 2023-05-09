e0b9baa2aaf206b42d6c8d3df4065de60b0e3971
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-05-09 10:31:34 -0600

    stepper completo

--- src/app/components/spp/spp.component.ts
+++ src/app/components/spp/spp.component.ts
@@ -1,6 +1,11 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup,Validators} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import {MatStepperIntl} from '@angular/material/stepper';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {StepperOrientation} from '@angular/material/stepper';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-spp',
@@ -8,7 +13,13 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
   styleUrls: ['./spp.component.css']
 })
 export class SppComponent {
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder, private _matStepperIntl: MatStepperIntl,breakpointObserver: BreakpointObserver) {
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+  }
+  optionalLabelText!: string;
+  optionalLabelTextChoices: string[] = ['Option 1', 'Option 2', 'Option 3'];
   firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
   secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
 
@@ -18,7 +29,17 @@ export class SppComponent {
   secondFormGroup1 = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
   });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+  stepperOrientation!: Observable<StepperOrientation>;
   isEditable = false;
-
-  
+  isOptional = false;
+  isLinear = false;
+  updateOptionalLabel() {
+    this._matStepperIntl.optionalLabel = this.optionalLabelText;
+    // Required for the optional label text to be updated
+    // Notifies the MatStepperIntl service that a change has been made
+    this._matStepperIntl.changes.next();
+  }
 }
